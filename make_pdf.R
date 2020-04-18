4cf0836dccb4e792ab1fb5dd836913ca5615e7f4
Simon Grundt Straubinger <simon.wismar@gmail.com>
2020-04-18 16:27:27 +0200

    Minor update

--- make_pdf.R
+++ make_pdf.R
@@ -1,1 +1,5 @@
+# CV to GitHub
 pagedown::chrome_print("straubinger-cv.html", "straubinger-cv.pdf")
+
+# CV to website
+pagedown::chrome_print("straubinger-cv.html", "C:/Users/simon/Dropbox/Rstats/website/static/files/straubinger-cv.pdf")
