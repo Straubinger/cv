9f62c5703a96755e39271aca46061a3350399bba
Simon Grundt Straubinger <simon.wismar@gmail.com>
2020-04-25 17:17:33 +0200

    Add website

--- make_pdf.R
+++ make_pdf.R
@@ -2,4 +2,4 @@
 pagedown::chrome_print("straubinger-cv.html", "straubinger-cv.pdf")
 
 # CV to website
-pagedown::chrome_print("straubinger-cv.html", "C:/Users/simon/Dropbox/Rstats/website/static/files/straubinger-cv.pdf")
+pagedown::chrome_print("straubinger-cv.html", "C:/Users/simon/OneDrive/Dokumenter/GitHub/website/static/files/straubinger-cv.pdf")
