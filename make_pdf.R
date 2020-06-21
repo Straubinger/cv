4c79d28bb2ee1e89fef85dec0f3223174e7722e9
Simon Grundt Straubinger <simon.wismar@gmail.com>
2020-06-21 18:25:02 +0200

    Update Danish resume

--- make_pdf.R
+++ make_pdf.R
@@ -1,5 +1,6 @@
 # CV to GitHub
-pagedown::chrome_print("straubinger-cv.html", "straubinger-cv.pdf")
+pagedown::chrome_print("straubinger-cv_da.html", "straubinger-cv_da.pdf")
+pagedown::chrome_print("straubinger-cv_en.html", "straubinger-cv_en.pdf")
 
 # CV to website
-pagedown::chrome_print("straubinger-cv.html", "C:/Users/simon/OneDrive/Dokumenter/GitHub/website/static/files/straubinger-cv.pdf")
+pagedown::chrome_print("straubinger-cv_en.html", "C:/Users/simon/OneDrive/Dokumenter/GitHub/website/static/files/straubinger-cv.pdf")
